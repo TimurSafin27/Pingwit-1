608ea42ec9f2b6465c173f939103aa98f40458ff
timursafin27 <timur270388@gmail.com>
2024-09-21 22:57:58 +0500

    23412

---
+++ src/main/java/org/example/lec_1/TypesCastingExample.java
@@ -0,0 +1,4 @@
+package org.example.lec_1;
+
+public class TypesCastingExample {
+}
---
+++ src/main/java/org/example/lec_1/TypesExample.java
@@ -0,0 +1,19 @@
+package org.example.lec_1;
+
+public class TypesExample {
+    public static void main(String[] args) {
+        byte myByte = 127;
+        int myInt = 128;
+
+        char myChar = 1000;
+        char mySimbolChar = 'k';
+        char myCharUtf = '\u33F4';
+        System.out.println(myChar);
+        System.out.println(mySimbolChar);
+        System.out.println(myCharUtf);
+        System.out.println("㏴");
+
+        boolean myBoolean;
+        System.out.print(myBoolean);
+    }
+}
