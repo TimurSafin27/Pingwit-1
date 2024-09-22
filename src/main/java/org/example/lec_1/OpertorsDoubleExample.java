27c78b949f7de3b6388e21d7c61fb99d94ad01b0
timursafin27 <timur270388@gmail.com>
2024-09-22 11:06:49 +0500

    23412

---
+++ src/main/java/org/example/lec_1/OpertorsDoubleExample.java
@@ -0,0 +1,22 @@
+package org.example.lec_1;
+
+public class OpertorsDoubleExample {
+    public static void main(String[] args) {
+        double d_1 = 100.0;
+        double d_2 = 100.0;
+
+        System.out.println(d_1 + d_2);
+        System.out.println(1.45 + 1.58);
+
+        System.out.println(3 / 2);
+        System.out.println(3 / 2.0);
+        System.out.println(3.0 / 2.0);
+
+        System.out.println(4.35 * 100);
+
+        System.out.println(1.45 % 1.55);
+
+        d_1--;
+        System.out.println(d_1);
+    }
+}
